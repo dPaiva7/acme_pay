768ace7ee67f651a53857a2c0e5c077493011d74
Diego <diegopaiva04@gmail.com>
2024-04-27 10:02:09 -0300

    [FIX] Atribuindo responsabilidades na classe Account

--- src/main/java/br/com/acmepay/Domain/Account.java
+++ src/main/java/br/com/acmepay/Domain/Account.java
@@ -1,11 +1,13 @@
 package br.com.acmepay.Domain;
 
+import br.com.acmepay.Exception.BalanceToWithdrawException;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 @Data
@@ -21,9 +23,44 @@ public class Account {
     private LocalDateTime created_at;
     private LocalDateTime update_at;
     private Boolean closed;
-    private BigDecimal createAcount;
 
-    public Account create() {
-        return this;
+    private List<Account> createAcount = new ArrayList<>();
+
+    public Account create(Account  accout) {
+        this.setId(1L);
+        this.setNumber(accout.number);
+        this.setAgency(accout.agency);
+        this.setBalance(BigDecimal.ZERO);
+        this.setCards(new ArrayList<>());
+        this.setCustomer(null);
+        this.setCreated_at(LocalDateTime.now());
+        this.setUpdate_at(null);
+        this.setClosed(Boolean.FALSE);
+        this.createAcount.add(this);
+
+        return accout;
     }
+
+    public void deposit(BigDecimal amount) {
+        this.balance.add(amount);
+    }
+
+    public void withdraw(BigDecimal amount)  throws BalanceToWithdrawException {
+        if (this.balance.compareTo(amount) >= 0){
+            this.balance.subtract(amount);
+        } else {
+            throw new BalanceToWithdrawException("Transaction failed");
+        }
+
+    }
+
+    public void transfer(BigDecimal amount, Account accountDest) throws BalanceToWithdrawException {
+        if (this.balance.compareTo(amount) >= 0){
+            this.withdraw(amount);
+            accountDest.deposit(amount);
+        } else {
+            throw new BalanceToWithdrawException("Transaction failed");
+        }
+    }
+
 }
---
+++ src/main/java/br/com/acmepay/Exception/BalanceToWithdrawException.java
@@ -0,0 +1,8 @@
+package br.com.acmepay.Exception;
+
+public class BalanceToWithdrawException extends Throwable {
+
+    public BalanceToWithdrawException(String message) {
+        super();
+    }
+}
